1edfc517dd53c7db2e6136f285e3f4ecb455a683
Sven Gonschorek <sven.gonschorek@bi-solutions-hamburg.com>
2024-08-18 18:09:35 +0200

    master: move schedules to schedules module

--- dags/definitions/definitions.py
+++ dags/definitions/definitions.py
@@ -1,20 +1,16 @@
 import os
 
-from dagster import Definitions, ScheduleDefinition, define_asset_job
+from dagster import Definitions
 from dagster_dbt import DbtCliResource
 
 from .assets import dwh_dbt_assets, airbyte_assets
 from .constants import dbt_project_dir
+from .schedules import schedules
 
 defs = Definitions(
     assets = [airbyte_assets, dwh_dbt_assets],
     resources = {
         "dbt": DbtCliResource(project_dir = os.fspath(dbt_project_dir))
     },
-    schedules = [
-            ScheduleDefinition(
-                job = define_asset_job("all_assets", selection = "*"),
-                cron_schedule = "@daily"
-            )
-    ]
+    schedules = schedules
 )
--- dags/definitions/schedules.py
+++ dags/definitions/schedules.py
@@ -1,15 +1,12 @@
 """
 To add a daily schedule that materializes your dbt assets, uncomment the following lines.
 """
+from dagster import ScheduleDefinition, define_asset_job
 from dagster_dbt import build_schedule_from_dbt_selection
 
-from .assets import dwh_dbt_assets
-
 schedules = [
-#     build_schedule_from_dbt_selection(
-#         [dwh_dbt_assets],
-#         job_name="materialize_dbt_models",
-#         cron_schedule="0 0 * * *",
-#         dbt_select="fqn:*",
-#     ),
+    ScheduleDefinition(
+        job = define_asset_job("all_assets", selection = "*"),
+        cron_schedule = "@daily"
+    )
 ]
